79534feaddad60b72d86095641de44427524efbc
Carole <carole@caroles-imac.attlocal.net>
2020-01-18 10:10:10 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+For use with grep-reddit or any other CLI utility, creates word histograms from stdin
---
+++ word_hist.py
@@ -0,0 +1,30 @@
+import sys
+from collections import Counter
+import numpy as np
+import matplotlib.pyplot as plt
+
+word_list = [line.rstrip('\r\n') for line in sys.stdin.readlines()]
+
+counts = Counter(word_list)
+
+labels, values = zip(*counts.items())
+
+# sort your values in descending order
+indSort = np.argsort(values)[::-1]
+
+# rearrange your data
+labels = np.array(labels)[indSort]
+values = np.array(values)[indSort]
+
+indexes = np.arange(len(labels))
+
+bar_width = 0.35
+
+plt.bar(indexes, values)
+
+# add labels
+plt.xticks(indexes + bar_width, labels)
+plt.show()
+
+
+
